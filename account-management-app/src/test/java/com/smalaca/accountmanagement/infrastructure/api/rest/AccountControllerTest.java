55bc29f66a71c743ca4980414b585d83e5100b25
smalaca <ab13.krakow@gmail.com>
2020-05-22 10:57:54 +0200

    CDC NEW DEVELOPMENT - adjusts tests of AccountController to meet new requirements

--- account-management-app/src/test/java/com/smalaca/accountmanagement/infrastructure/api/rest/AccountControllerTest.java
+++ account-management-app/src/test/java/com/smalaca/accountmanagement/infrastructure/api/rest/AccountControllerTest.java
@@ -6,6 +6,7 @@ import com.smalaca.accountmanagement.domain.account.AccountRepository;
 import com.smalaca.accountmanagement.domain.customer.Customer;
 import com.smalaca.accountmanagement.domain.customer.CustomerRepository;
 import com.smalaca.accountmanagement.infrastructure.api.rest.dto.TransferDto;
+import com.smalaca.accountmanagement.infrastructure.currencyapp.CurrencyApplicationClient;
 import com.smalaca.cdc.contract.ContractReader;
 import com.smalaca.cdc.contract.Input;
 import com.smalaca.cdc.contract.Output;
@@ -15,60 +16,66 @@ import java.math.BigDecimal;
 import java.util.Optional;
 import java.util.UUID;
 
-import static com.smalaca.accountmanagement.domain.currency.Currency.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 
 class AccountControllerTest {
-    private static final String SOME_DATE = "2020-01-01";
+    private static final int EUR_TO_PLN = 0;
+    private static final int PLN_TO_USD = 1;
+    private static final int UNKNOWN_CURRENCY = 2;
 
+    private final CurrencyApplicationClient currencyApplicationClient = mock(CurrencyApplicationClient.class);
     private final AccountRepository accountRepository = mock(AccountRepository.class);
     private final CustomerRepository customerRepository = mock(CustomerRepository.class);
     private final AccountFactory accountFactory = mock(AccountFactory.class);
     private final AccountController accountController = new AccountController(accountRepository, customerRepository, accountFactory);
+    private final ContractReader contractReader = new ContractReader();
 
     @Test
     void shouldTransferMoneyFromEurAccountToPlnAccount() {
-        Account from = existingAccount(EUR.name());
-        Account to = existingAccount(PLN.name());
-        TransferDto transferDto = transferDto(from, to, 100);
+        Input input = givenExchangeFor(EUR_TO_PLN);
+        Account from = existingAccount(input.getFrom());
+        Account to = existingAccount(input.getTo());
+        TransferDto transferDto = transferDto(from, to, input.getAmount(), input.getDate());
 
         String actual = accountController.transfer(transferDto);
 
         assertThat(actual).isEqualTo("SUCCESS");
         assertThat(from.asDto().getBalance()).isEqualTo(BigDecimal.valueOf(900));
-        assertThat(to.asDto().getBalance()).isEqualTo(BigDecimal.valueOf(1100));
+        assertThat(to.asDto().getBalance()).isEqualTo(BigDecimal.valueOf(1400));
     }
 
     @Test
     void shouldTransferMoneyFromPlnAccountToEurAccount() {
-        Account from = existingAccount(PLN.name());
-        Account to = existingAccount(USD.name());
-        TransferDto transferDto = transferDto(from, to, 900);
+        Input input = givenExchangeFor(PLN_TO_USD);
+        Account from = existingAccount(input.getFrom());
+        Account to = existingAccount(input.getTo());
+        TransferDto transferDto = transferDto(from, to, input.getAmount(), input.getDate());
 
         String actual = accountController.transfer(transferDto);
 
         assertThat(actual).isEqualTo("SUCCESS");
         assertThat(from.asDto().getBalance()).isEqualTo(BigDecimal.valueOf(100));
-        assertThat(to.asDto().getBalance()).isEqualTo(BigDecimal.valueOf(1900));
+        assertThat(to.asDto().getBalance()).isEqualTo(BigDecimal.valueOf(2800));
     }
 
     @Test
     void shouldTransferMoneyFromAccountWithUnknownCurrency() {
-        Account from = existingAccount(RUB.name());
-        Account to = existingAccount(USD.name());
-        TransferDto transferDto = transferDto(from, to, 200);
+        Input input = givenExchangeFor(UNKNOWN_CURRENCY);
+        Account from = existingAccount(input.getFrom());
+        Account to = existingAccount(input.getTo());
+        TransferDto transferDto = transferDto(from, to, input.getAmount(), input.getDate());
 
         String actual = accountController.transfer(transferDto);
 
-        assertThat(actual).isEqualTo("SUCCESS");
-        assertThat(from.asDto().getBalance()).isEqualTo(BigDecimal.valueOf(800));
-        assertThat(to.asDto().getBalance()).isEqualTo(BigDecimal.valueOf(1200));
+        assertThat(actual).isEqualTo("FAILED");
+        assertThat(from.asDto().getBalance()).isEqualTo(BigDecimal.valueOf(1000));
+        assertThat(to.asDto().getBalance()).isEqualTo(BigDecimal.valueOf(1000));
     }
 
-    private TransferDto transferDto(Account from, Account to, long amount) {
-        return new TransferDto(from.accountNumber(), to.accountNumber(), BigDecimal.valueOf(amount), SOME_DATE);
+    private TransferDto transferDto(Account from, Account to, long amount, String date) {
+        return new TransferDto(from.accountNumber(), to.accountNumber(), BigDecimal.valueOf(amount), date);
     }
 
     private Account existingAccount(String currency) {
@@ -80,5 +87,12 @@ class AccountControllerTest {
 
     private String login() {
         return UUID.randomUUID().toString();
+    }
+
+    private Input givenExchangeFor(int scenario) {
+        Input input = contractReader.inputForScenario(scenario);
+        Output output = contractReader.outputForScenario(scenario);
+        given(currencyApplicationClient.exchange(input)).willReturn(output);
+        return input;
     }
 }
